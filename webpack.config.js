ba5f6e6ce039033667a7871de316c78e80ac920d
jimmy <jimmy0328@gmail.com>
2017-07-07 15:04:26 +0800

    setting html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
 module.exports = {
   entry: './client/index.js',
   output: {
