ea230824ff90524d01e3e5b401eb3258712437c6
jimmy <jimmy0328@gmail.com>
2017-07-07 17:30:37 +0800

    Fix Unknown props onTouchTap warnings

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,7 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div style={{textAlign: 'center'}}>
-        <RaisedButton label="Default" />
+        <RaisedButton label="Hello World" />
       </div>);
   }
 }
